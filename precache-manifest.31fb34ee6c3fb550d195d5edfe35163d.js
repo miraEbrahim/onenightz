df6b4568a5c038496d9de4a382b13d3dd571208c
miraEbrahim <mxebrahim@gmail.com>
2019-01-28 14:22:44 +0000

    Updates

---
+++ precache-manifest.31fb34ee6c3fb550d195d5edfe35163d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6587d9ffc7539dd6d386",
+    "url": "/miraEbrahim/onenightz.git/static/css/main.7b096535.chunk.css"
+  },
+  {
+    "revision": "6587d9ffc7539dd6d386",
+    "url": "/miraEbrahim/onenightz.git/static/js/main.6587d9ff.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/miraEbrahim/onenightz.git/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0d144caae18e21c9261a",
+    "url": "/miraEbrahim/onenightz.git/static/js/runtime~main.0d144caa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miraEbrahim/onenightz.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4a0e46ceee657d90a50a8ddf631029a7",
+    "url": "/miraEbrahim/onenightz.git/index.html"
+  }
+];
